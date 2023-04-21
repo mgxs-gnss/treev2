6f918dc9aab83292b7de3683a620ca1ff0405cdf
Silvio Paganini <paganini.silvio@bcgplatinion.com>
2023-04-21 15:30:52 -0300

    slider

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
 import { Box, CircularProgress } from "@mui/material";
 import { useEffect, useState } from "react";
+import { Autoplay, EffectCards, Mousewheel } from "swiper";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./home.css";
+import { FreeMode } from "swiper";
 
 const Home = () => {
   const [images, setImages] = useState<string[]>();
@@ -21,43 +27,53 @@ const Home = () => {
     load();
   }, [setImages]);
 
-  const w = 1152;
-  const h = 1568;
-  const perc = 0.3;
+  const size = 80;
+  const ratio = 1152 / 1568;
 
   return (
-    <Box
-      display="grid"
-      maxWidth="100vw"
-      overflow="hidden"
-      gap={1}
-      gridTemplateColumns={`repeat( auto-fit, minmax(${w * perc}px, 1fr) )`}
-      justifyItems="center"
-    >
-      {!images && (
-        <CircularProgress
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
+    <>
+      {/* {!images && ( */}
+      <CircularProgress
+        sx={{
+          position: "absolute",
+          top: "calc(50% - 20px)",
+          left: "calc(50% - 20px)",
+        }}
+      />
+      {/* )} */}
+      {images && images.length > 0 && (
+        <Swiper
+          style={{ transform: "rotate(90deg)" }}
+          effect={"cards"}
+          grabCursor
+          centeredSlides
+          freeMode
+          cardsEffect={{
+            slideShadows: false,
+            perSlideRotate: 10,
+            perSlideOffset: 10,
           }}
-        />
+          autoplay={{ pauseOnMouseEnter: true, delay: 2000 }}
+          mousewheel
+          modules={[EffectCards, FreeMode, Autoplay, Mousewheel]}
+        >
+          {images &&
+            images?.map((i, key) => (
+              <SwiperSlide key={key}>
+                <Box
+                  sx={{
+                    backgroundImage: `url(${i})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    height: `${Math.round(size / ratio)}vw`,
+                    width: `${size}vw`,
+                  }}
+                />
+              </SwiperSlide>
+            ))}
+        </Swiper>
       )}
-      {images &&
-        images?.map((i, key) => (
-          <Box
-            sx={{
-              backgroundImage: `url(${i})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              width: "100%",
-              height: `${h * perc}px`,
-            }}
-            key={key}
-          />
-        ))}
-    </Box>
+    </>
   );
 };
 
