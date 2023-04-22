fc5e9d98a302c7fc854f62ef67236720d133f00e
Silvio Paganini <s2paganini@gmail.com>
2023-04-22 11:05:07 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -47,7 +47,6 @@ const Home = () => {
         <Swiper
           style={{ transform: "rotate(90deg)" }}
           effect={"cards"}
-          grabCursor
           centeredSlides
           freeMode
           cardsEffect={{
