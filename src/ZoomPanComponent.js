40f76d9d74e338c0864cb198df2fab0d77f3012a
mgxsco <fernando@mglhs.com>
2023-05-11 20:22:26 +0100

    mems count

--- src/ZoomPanComponent.js
+++ src/ZoomPanComponent.js
@@ -4,29 +4,40 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const ZoomPanComponent = () => {
   const [images, setImages] = useState({ jsonFiles: [], jpgFiles: [] });
+    const [jsonCount, setJsonCount] = useState(0);  // New state variable for JSON count
   const [highlightedImageJson, setHighlightedImgJson] = useState(null);
-  // eslint-disable-next-line
-  const [highlightedImageSrc, setHighlightedImgSrc] = useState(null);
-  const [updatePending, setUpdatePending] = useState(false);  // new state variable
   const imageRefs = useRef([]);
 
-    useEffect(() => {
-      fetch('https://api.mgxs.co/mem/list')
-        .then(response => response.json())
-        .then(data => {
-          // Filter only the JSON files
-          const jsonFiles = data.filter(file => file.endsWith('.json'));
-          // Filter only the JPG files
-          const jpgFiles = data.filter(file => file.endsWith('low.jpg'));
-          setImages({ jsonFiles, jpgFiles });
-        });
-    }, []);
 
-// eslint-disable-next-line
+  useEffect(() => {
+    const fetchImages = async () => {
+      const response = await fetch('https://api.mgxs.co/mem/list');
+      const data = await response.json();
+
+      const jsonFiles = data.filter(file => file.endsWith('.json'));
+      const jpgFiles = data.filter(file => file.endsWith('low.jpg'));
+
+        const jsonPromises = jsonFiles.map(jsonFile =>
+          fetch(jsonFile).then(response => response.json())
+        );
+
+      const jsonData = await Promise.all(jsonPromises);
+      setImages({ jsonFiles: jsonData, jpgFiles });
+      setJsonCount(jsonData.length);
+
+      console.log(`Number of JSON files: ${jsonData.length}`);  // Added this line
+    };
+
+    fetchImages();
+    const intervalId = setInterval(fetchImages, 1000);  // 10000 ms = 10 seconds
+
+    return () => clearInterval(intervalId); // Clear interval on unmount
+  }, []);
+
   const highlightClosestImage = () => {
     const center = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
     let minDistance = Infinity;
-    let closestImage = null;
+    let closestIndex = 0;
 
     imageRefs.current.forEach((ref, index) => {
       if (ref) {
@@ -35,69 +46,74 @@ const ZoomPanComponent = () => {
         const dx = center.x - imageCenter.x;
         const dy = center.y - imageCenter.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-            if (distance < minDistance) {
-              minDistance = distance;
-              closestImage = ref;
-              setHighlightedImgSrc(images.jpgFiles[index]);
 
-              const jsonFile = images.jsonFiles.find(file => file.name === images.jpgFiles[index]);
-              setHighlightedImgJson(jsonFile);
-
-              setUpdatePending(true);
-            }
+        if (distance < minDistance) {
+          minDistance = distance;
+          closestIndex = index;
+        }
       }
     });
-    if (updatePending) {
-      setUpdatePending(false);  // reset updatePending
-    }
+
+if (closestIndex !== null) {
+  const jsonFile = images.jsonFiles[closestIndex];
+  if (jsonFile) {
+    setHighlightedImgJson(jsonFile);
+  } else {
+    console.log(`No JSON file for image at index: ${closestIndex}`);
+  }
+} else {
+  console.log(`No closest image found.`);
+}
 
     imageRefs.current.forEach((ref) => {
       if (ref) {
-        ref.style.transform = ref === closestImage ? 'scale(1.4)' : 'scale(1)'; // scale up the highlighted image
-        ref.style.boxShadow = ref === closestImage ? '50px 50px 50px black' : '0px 0px 0px black';
-        ref.style.zIndex = ref === closestImage ? '1' : '0'; // bring the highlighted image to the front
-        ref.style.transition = 'transform, boxShadow 0.1s ease-in-out'; // add transition effect
-        ref.style.opacity = ref === closestImage ? '1' : '.9';
-        //ref.style.maxScale = ref === closestImage ? '100px' : '500px';
-       // ref.style.objectFit = ref === closestImage ? 'scale-down' : 'none';
-        ref.style.outline = ref === closestImage ? '1rem solid gray' : 'none'; // highlight the closest image
+
+        const isHighlighted = ref === imageRefs.current[closestIndex];
+        ref.style.transform = isHighlighted ? 'scale(1.4)' : 'scale(1)';
+        ref.style.boxShadow = isHighlighted ? '50px 50px 50px black' : '0px 0px 0px black';
+        ref.style.zIndex = isHighlighted ? '1' : '0';
+        ref.style.transition = 'transform, boxShadow 0.1s ease-in-out';
+        ref.style.opacity = isHighlighted ? '1' : '.9';
+        ref.style.outline = isHighlighted ? '1rem solid gray' : 'none';
       }
     });
   };
   useEffect(() => {
-        document.body.style.backgroundColor = 'black';
-        document.body.style.margin = '0';// set the body background color to black
-    return () => { // cleanup function
-      document.body.style.backgroundColor = null; // reset the body background color when component unmounts
+    document.body.style.backgroundColor = 'black';
+    document.body.style.margin = '0';
+
+    return () => { // Cleanup function
+      document.body.style.backgroundColor = null;
     };
   }, []);
-// Add this useEffect hook
-useEffect(() => {
-  if (images.length > 0) {
-    highlightClosestImage();
-  }
-// eslint-disable-next-line
-}, [images, highlightClosestImage]); // Added highlightClosestImage
-useEffect(() => {
-  if (updatePending) {
-    setUpdatePending(false); // reset updatePending, causing a re-render
-  }
-}, [updatePending]);
 
   return (
     <>
-        {highlightedImageJson && (
-          <div style={{ backgroundColor:'gray', color: 'black',width:'30%', height:'20%', padding: '20px', position: 'absolute', bottom: '0px', right: '0px', zIndex: 2}}>
-            {highlightedImageJson.attributes.map((attribute, index) => (
-              <p key={index}>{attribute.trait_type}: {attribute.value}</p>
-            ))}
-          </div>
-        )}
+          <div style={{ backgroundColor: 'gray', color: 'black', width: '20%', padding: '20px', position: 'absolute', top: '0px', left: '0px', zIndex: 2}}>
+        Number of MEMs: {jsonCount}
+      </div>
+              <div
+                style={{ backgroundColor: 'gray', color: 'black', width: '30%', height: '100%', padding: '20px', position: 'absolute', bottom: '0px', right: '0px', zIndex: 2}}
+
+                // Assuming `name` uniquely identifies each JSON file
+              >
+                  {highlightedImageJson && highlightedImageJson.attributes && highlightedImageJson.attributes.length > 0 ? (
+                    highlightedImageJson.attributes.map((attribute, index) => (
+                      <p key={index}>{attribute.trait_type}: {attribute.value}</p>
+                    ))
+                  ) : (
+                    <p>No highlighted JSON file</p>
+                  )}
+                </div>
+            )}
       <TransformWrapper
         initialScale={2}
         maxScale={10}
         minScale={0.1}
         limitToBounds={0}
+        initialPositionX={20}
+        initialPositionY={20}
+        onInit={highlightClosestImage}
         onPanning={highlightClosestImage}
         onZoom={highlightClosestImage}
       >
@@ -105,8 +121,11 @@ useEffect(() => {
           <React.Fragment>
             <TransformComponent>
               <div style={{ display: 'grid', gridTemplateColumns: `repeat(16, 1fr)`, gap: '20px', width: '100%', height: '100vh', backgroundColor: 'black' }}>
-                {images.jpgFiles.map((src, index) => (
-                  <img loading="lazy" key={index} src={src} alt={`Example ${index}`} style={{ objectFit: 'cover', width: '100%' }} ref={el => imageRefs.current[index] = el}/>
+                {images.jpgFiles.length > 0 &&
+                  <img loading="lazy" key={0} src={images.jpgFiles[0]} alt={`Example 0`} style={{ objectFit: 'cover', width: '100%' }} ref={el => imageRefs.current[0] = el}/>
+                }
+                {images.jpgFiles.slice(1).map((src, index) => (
+                  <img loading="lazy" key={index + 1} src={src} alt={`Example ${index + 1}`} style={{ objectFit: 'cover', width: '100%' }} ref={el => imageRefs.current[index + 1] = el}/>
                 ))}
               </div>
             </TransformComponent>
@@ -117,4 +136,4 @@ useEffect(() => {
   );
 };
 
-export default ZoomPanComponent
+export default ZoomPanComponent;
